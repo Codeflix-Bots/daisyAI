9684ef31aa7e1562fa785f359e4fd17f779c5e87
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-24 11:04:18 +0530

    Create __init__.py

---
+++ Utils/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2023 CodeFlix_Bots, MIT License
+
+from .API import *
+from .btnParser import *
+from .miscs import *
+from .pastebins import *
+from .telegraph import *
+from .htmlParser import *
+from .decorators import *
