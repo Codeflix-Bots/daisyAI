c8bd2175cfa249d77603069cf9d8ce3b0a3f2588
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-24 11:06:33 +0530

    Create pastebins.py

---
+++ Utils/pastebins.py
@@ -0,0 +1,27 @@
+import httpx
+
+NEKOBIN = "https://nekobin.com/api/documents"
+async def nekobin(data,extension=None):
+    """
+    To Paste the given message/text/code to nekobin
+    """
+    try:
+        async with httpx.AsyncClient() as req:
+            res = req.post(
+                url=NEKOBIN,
+                json={
+                    "content":data,
+                    "title": "data",
+                    "author": "SDWaifuRobot"
+                })
+    except Exception as e:
+        return {"error": str(e)}
+    if res.ok:
+        resp = res.json()
+        purl = (
+            f"nekobin.com/{resp['result']['key']}.{extension}"
+            if extension
+            else f"nekobin.com/{resp['result']['key']}"
+        )
+        return purl
+    return {"error": "Unable to reach nekobin."}
