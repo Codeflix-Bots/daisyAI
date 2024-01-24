a76c591363eca5c6132b7d78a7b9f1d5a6cfde2e
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-24 12:34:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,4 @@ class Config(object):
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     API_ID = os.getenv("API_ID")
     API_HASH = os.getenv("API_HASH")
-    SUPPORT_CHAT_ID = os.getenv("SUPPORT_CHAT_ID")
-    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "AIzaSyD214hhYJ-xf8rfaWX044_g1VEBQ0ua55Q")
-    LOG_CHANNEL = os.getenv('LOG_CHANNEL', '-1001868871195')
     mediaPattern = r"\b(https?://(?:(.*?)\.)?(?:instagram\.com|instagr\.am|t\.co|twitter\.com)(?:[^\s]*))\b" #pin\.it|pinterest\.com|
