2c7306e8e6fa6cd79bcfb34cbca4a2c2b7298f17
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-24 12:25:16 +0530

    Update ask.py

--- plugins/ask.py
+++ plugins/ask.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import requests
-from Config import LOG_CHANNEL, GOOGLE_API_KEY, 
+from Config import LOG_CHANNEL, GOOGLE_API_KEY, SUPPORT_CHAT_ID 
 import google.generativeai as genai
 
 genai.configure(api_key=GOOGLE_API_KEY)
